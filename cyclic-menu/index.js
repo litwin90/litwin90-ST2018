b64abffd7213b0824e10f355ae607727db70267b
Dzianis Davydau <dde0791@gmail.com>
2018-11-20 18:59:17 +0300

    implement setActiveClassTo method in Menu class

--- cyclic-menu/index.js
+++ cyclic-menu/index.js
@@ -4,3 +4,7 @@ import Menu from './src/Menu.js';
 const menuElement = new Menu(['Home', 'Domain', 'Hosting', 'Clients', 'Blog', 'Support']);
 const parent = document.querySelector('body');
 menuElement.append(parent);
+
+setTimeout(menuElement.setActiveClassTo.bind(menuElement), 3000, 3);
+
+menuElement.setActiveClassTo(2);
--- cyclic-menu/src/Menu.js
+++ cyclic-menu/src/Menu.js
@@ -19,4 +19,20 @@ export default class Menu {
     append(parent = document.querySelector('body')) {
         parent.appendChild(this.menuContainer);
     }
+
+    setActiveClassTo(menuElementNumber = 0) {
+        let num = menuElementNumber;
+        if (typeof num === 'number' && !Number.isNaN(num)) {
+            if (num < 0) {
+                num = 0;
+            } else if (num > this.items.length) {
+                num %= this.items.length;
+            }
+        } else {
+            num = 0;
+        }
+        this.items[this.activeItem].classList.remove('active');
+        this.activeItem = menuElementNumber;
+        this.items[this.activeItem].classList.toggle('active');
+    }
 }
