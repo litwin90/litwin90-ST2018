b37f384b3b39ffbed2800370d1a5e50886691356
Dzianis Davydau <dde0791@gmail.com>
2018-12-14 14:37:02 +0300

    create server

--- wunderlist/backEnd/app.js
+++ wunderlist/backEnd/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const viewPath = path.resolve(__dirname, '../frontEnd/views/');
+const stylePaht = path.resolve(__dirname, '../frontEnd/style/');
+const srcPath = path.resolve(__dirname, '../frontEnd/src/');
+app.use(express.static(viewPath));
+app.use(express.static(stylePaht));
+app.use(express.static(srcPath));
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile('/index.html');
 });
 
-app.listen(3000, () => {
-    // console.log('Example app listening on port 3000!');
+
+app.use(express.static(viewPath));
+app.use(express.static(stylePaht));
+app.use(express.static(srcPath));
+
+app.get('/register', (req, res) => {
+    res.sendFile('./register.html', { root: viewPath });
 });
+
+app.listen(3000);
