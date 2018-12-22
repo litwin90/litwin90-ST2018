b7bf14534da9cfd04eb06964bfc3b050b7841c8f
Dzianis Davydau <dde0791@gmail.com>
2018-12-22 20:49:15 +0300

    create start template on client

---
+++ wunderlist/client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Form from '../components/Form';
+
+class App extends Component {
+  render() {
+    return (
+        <Form className="App">
+            form
+        </Form>
+    );
+  }
+}
+
+export default App;
---
+++ wunderlist/client/src/components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = ({name}) => (
+    <form className="register-form">
+        form
+    </form>
+); 
+
+export default Form;
