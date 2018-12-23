1e602a0122e2b9b1084a54fcb2de4d8597199166
Dzianis Davydau <dde0791@gmail.com>
2018-12-23 10:24:35 +0300

    add validation ti signup form

---
+++ wunderlist/client/src/components/FormSignUp.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import Input from './Input';
+import FormControl from './FormControl';
+
+class FormSignUp extends React.Component {
+    state = {
+        firstName: '',
+        firstNameError: '',
+        password: '',
+        passwordError: '',
+        passwordRepeate: '',
+        passwordRepeateError: '',
+    };
+
+    onFirstNameChange = event => {
+        this.setState({
+            firstName: event.target.value,
+        });
+    }
+
+    onPasswordChange = event => {
+        this.setState({
+            password: event.target.value,
+        });
+    }
+    
+    onPasswordRepeateChange = event => {
+        this.setState({
+            passwordRepeate: event.target.value,
+        });
+    }
+
+
+
+    onFirstNameBlur = () => {
+        const { firstName } = this.state;
+    
+        const firstNameError = this.validateName( firstName );
+    
+        return this.setState({ firstNameError });
+    };
+
+    onPasswordBlur = () => {
+        const { password } = this.state;
+    
+        const passwordError = this.validatePassword( password );
+    
+        return this.setState({ passwordError });
+    };
+
+    onPasswordRepeatBlur = () => {
+        const { passwordRepeate } = this.state;
+    
+        const passwordRepeateError = this.validatePassword( passwordRepeate );
+    
+        return this.setState({ passwordRepeateError });
+    };
+
+    validateName = name => {
+        const regex = /[A-Za-z]{3,}/;
+
+        return !regex.test(name)
+            ? "Uncorrect user name"
+            : "";
+    }
+
+    validatePassword = password => {
+        const regex = /[A-Za-z]{3,}/;
+
+        return !regex.test(password)
+            ? "Uncorrect password"
+            : "";
+    }
+
+    validatePasswordsMatch = (password1, password2) => {
+        return (password1 === password2) ? ('') : ('Passwords should match');
+    }
+    
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { 
+            password,
+            passwordRepeate,
+        } = this.state;
+
+        const passwordRepeateError = this.validatePasswordsMatch(password, passwordRepeate);
+
+        return this.setState({ passwordRepeateError });
+    }
+
+    render() {
+        const {
+            firstNameError,
+            passwordError,
+            passwordRepeateError,
+            submitError,
+        } = this.state;
+        return (
+            <form 
+                className="App register-form"
+                onSubmit={this.onSubmit}
+                error = {submitError}
+            >
+                <div className="container">
+                    <h1>Register</h1>
+                    <p>Please fill in this form to create an account.</p>
+                    <hr/>
+                    <Input 
+                        header="User Name" 
+                        type="text" 
+                        name="username"
+                        plaseHolder="Enter user name" 
+                        onChange = {this.onFirstNameChange}
+                        onBlur = {this.onFirstNameBlur}
+                        error = {firstNameError}   
+                    ></Input>
+                    <Input header="Password" 
+                        type="password" 
+                        name="password"
+                        plaseHolder="Enter password"
+                        onChange = {this.onPasswordChange}
+                        onBlur = {this.onPasswordBlur}
+                        error = {passwordError} 
+                    ></Input>
+                    <Input header="Repeat Password" 
+                        type="password" 
+                        name="passwordRepeat"
+                        plaseHolder="Repeat password"
+                        onChange = {this.onPasswordRepeateChange}
+                        onBlur = {this.onPasswordRepeatBlur}
+                        error = {passwordRepeateError} 
+                    ></Input>
+                    <hr/>
+                </div>
+                <div className="container signin">
+                    <div className="git-hub"></div>
+                    <p>By creating an account you agree to our <a href="/">Terms & Privacy</a>.</p>
+                    <FormControl
+                        type="submit"
+                        value="Register" 
+                        className="registerbtn">
+                    </FormControl>
+                    <p>Already have an account? <a href="/">Sign in</a>.</p>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default FormSignUp;
--- wunderlist/client/src/components/Input.js
+++ wunderlist/client/src/components/Input.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 // props
-const Input = ({ header, type, name, plaseHolder }) => (
+const Input = ({ header, type, name, plaseHolder, onChange, onBlur, error }) => (
     <label><b>{header}</b>
         <input 
             type={type} 
             placeholder={plaseHolder}
             name={name}
+            onChange = {onChange}
+            onBlur = {onBlur}
+            className={(error) ? ('invalid') : ('')}
         />
+        <div className="errorContainer" style={{display: (error) ? ('block') : ('none')}}>
+            <p className="error">{error}</p>
+        </div>
     </label>
 );
 
